2eaa4852ac1811439217242d20cb3bd4d8090d86
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-10 15:46:02 -0400

    Update application_form.php

--- application_form.php
+++ application_form.php
@@ -20,18 +20,20 @@
     <form method="post">
       What degree are you applying for? (MS/PhD) <input type="text" name="degreeType"><br><br>
       
-      GRE<br>
+      GRE: <br>
       Verbal <span><input type="text" name="verbal"></span><br>
       Quantitative <span><input type="text" name="quantitative"></span><br>
       Year of exam <span><input type="text" name="year"></span><br><br>
-      GRE advanced <br>
+      GRE advanced: <br>
       Score <span><input type="text" name="advScore"></span><br>
       Subject <span><input type="text" name="subject"></span><br><br>
       TOEFL Score <span><input type="text" name="toefl"></span><br>
       Year of exam <span><input type="text" name="advYear"></span><br><br>
 
       Areas of Interest <span><input type="text" name="aoi"></span><br>
-      Experience <span><input type="text" name="experience"></span>
+      Experience <span><input type="text" name="experience"></span><br>
+      
+      <input type="submit" name="submit" value="Submit"> 
     </form>
       
   </body>
