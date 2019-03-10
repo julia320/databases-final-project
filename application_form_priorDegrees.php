2a5fbd3d51afbb1e65457d1169b89ba2c3b35187
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-10 16:42:33 -0400

    Create application_form_priorDegrees.php

---
+++ application_form_priorDegrees.php
@@ -0,0 +1,30 @@
+<html>
+  
+  <title>
+    Application Form
+  </title>
+  
+  <style>
+    span {
+      position: absolute;
+      left: 140px;
+    }
+    body{line-height: 1.6;}
+  </style>
+  
+   <h1> Application Form </h1>
+   <h3> Page 2: Prior Degrees </h3><br>
+  
+  <body>
+    
+    <form method="post">
+      Degree type (MS,BS, or BA)<span><input type="text" name="type"></span><br>
+      GPA <span><input type="text" name="gpa"></span><br>
+      Year <span><input type="text" name="year"></span><br>
+      University<span><input type="text" name="university"></span><br><br>
+      
+      <input type="submit" name="submit" value="Submit"> 
+    </form>
+      
+  </body>
+</html>
