85ff33363fb0ec95456ec94b923b6a38f96f4e09
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-10 15:05:21 -0400

    Update application_form.php

--- application_form.php
+++ application_form.php
@@ -8,10 +8,10 @@
       What degree are you applying for? (MS/PhD) <input type="text" name="degreeType"><br><br>
       
       Test Scores: <br>
-      GRE<pre>Verbal<input type="text" name="verbal"><pre>Quantitative<input type="text" name="quantitative"><br>
+      GRE &nbsp;&nbsp;&nbsp; Verbal<input type="text" name="verbal"> &nbsp;&nbsp;&nbsp; Quantitative<input type="text" name="quantitative"><br>
       Year of exam<input type="text" name="year"><br>
-      GRE advanced<pre>Score<input type="text" name="advScore"><pre><input type="text" name="subject"><br>
-      TOEFL Score<input type="text" name="toefl"><pre><pre>Year of exam<input type="text" name="advYear">
+      GRE advanced &nbsp;&nbsp;&nbsp; Score<input type="text" name="advScore"> &nbsp;&nbsp;&nbsp; <input type="text" name="subject"><br>
+      TOEFL Score<input type="text" name="toefl"> &nbsp;&nbsp;&nbsp; Year of exam<input type="text" name="advYear">
 
 
       
