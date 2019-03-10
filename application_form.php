b2ac0985faf21edeac1e17963f7dfc5a465a3654
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-10 15:13:01 -0400

    Update application_form.php

--- application_form.php
+++ application_form.php
@@ -2,18 +2,21 @@
   <title>
     Application Form
   </title>
+  <style>
+    .center {
+      text-align: center;
+    }
+  </style>
   <body>
     <h1> Application Form </h1>
     <form method="post">
       What degree are you applying for? (MS/PhD) <input type="text" name="degreeType"><br><br>
       
       Test Scores: <br>
-      GRE &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      Verbal<input type="text" name="verbal">&nbsp;&nbsp;&nbsp;Quantitative<input type="text" name="quantitative"><br>
-      Year of exam<input type="text" name="year"><br>
-      GRE advanced &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      Score<input type="text" name="advScore"> &nbsp;&nbsp;&nbsp; <input type="text" name="subject"><br>
-      TOEFL Score<input type="text" name="toefl"> &nbsp;&nbsp;&nbsp; Year of exam<input type="text" name="advYear">
+      GRE <span class="center">Verbal<input type="text" name="verbal"> Quantitative<input type="text" name="quantitative"><br>
+      Year of exam<input type="text" name="year"><br></span>
+      GRE advanced <span class="center">Score<input type="text" name="advScore"> Subject<input type="text" name="subject"><br>
+      TOEFL Score<input type="text" name="toefl">Year of exam<input type="text" name="advYear"></span><br>
 
 
       
