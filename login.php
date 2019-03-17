84e061783d8a00c764644a556781af779e95a1fe
Julia Bristow <julia_bristow@gwu.edu>
2019-03-16 23:29:24 -0400

    code and format outline for login page

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log In to APPS</title>
+	<!--<link rel="stylesheet" type="text/css" href="style.css">
+
+ 	CSS Styling -->	
+	<style>
+		/* 2 columns for logging in/signing up */
+		.row {
+			text-align: center;
+			display: flex;
+		}
+
+		.column {
+			text-align: center;
+			flex: 50%;
+		}
+
+	</style>	
+</head>
+
+<body>
+	<h1 style="text-align: center;">Graduate Application System</h1>
+	<div class="row">
+		<!-- Log in -->
+		<div class="column">
+			<h4>Log In</h4>
+			<p>Log in to complete your application, view its satus, or see the final decision</p>
+
+		</div>
+
+		<!-- Sign up -->
+		<div class="column">
+			<h4>Sign Up</h4>
+			<p>Sign up here if you don't already have an account to begin your application</p>
+		</div>
+	</div>
+</body>
+</html>
