78eaa419dac9070288d844fdd049ae8f0a85b697
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-18 08:19:40 -0400

    Create application_form_recommendation.php

---
+++ application_form_recommendation.php
@@ -0,0 +1,27 @@
+<html>
+  
+  <title>
+    Application Form
+  </title>
+  
+  <style>
+    span {
+      position: absolute;
+      left: 140px;
+    }
+    body{line-height: 1.6;}
+  </style>
+  
+   <h1> Application Form </h1>
+   <h2> Page 3: Recommendation Letter </h2>
+  
+  <body>
+    
+    <form method="post">
+      Give me name <span><input type="text" name="x"></span><br>
+      Give me email <span><input type="text" name="verbal"></span><br>
+      <input type="submit" name="submit" value="Submit"> 
+    </form>
+      
+  </body>
+</html>
