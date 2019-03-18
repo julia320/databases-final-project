b07febaeef16d17b75a9f646bb7598f282dbbf70
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-18 08:32:08 -0400

    Update application_form_recommendation.php

--- application_form_recommendation.php
+++ application_form_recommendation.php
@@ -14,12 +14,19 @@
   
    <h1> Application Form </h1>
    <h2> Page 3: Recommendation Letter </h2>
+   Enter the contact information of the person to provide your recommendation letter. 
+   We will reach out to this person and ask for their letter. 
+   See homepage to check the status of your recommendation letter.
   
   <body>
     
-    <form method="post">
-      Give me name <span><input type="text" name="x"></span><br>
-      Give me email <span><input type="text" name="verbal"></span><br>
+    <form id="mainform" action="application_form_end.php" method="post">
+      <h3>Recommendation Letter One(required)</h3>
+      First name <span><input type="text" name="fname"></span><br>
+      Last name <span><input type="text" name="lname"></span><br>
+      Institution/affiliation <input type="text" name="institution"></span><br>
+      email <input type="text" name="email"></span><br>
+      
       <input type="submit" name="submit" value="Submit"> 
     </form>
       
