78f8e83c157e94f2fcaaa951193928695e66f50e
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-18 08:11:44 -0400

    Update application_form_priorDegrees.php

--- application_form_priorDegrees.php
+++ application_form_priorDegrees.php
@@ -3,49 +3,69 @@
   <title>
     Application Form
   </title>
-  
+
   <style>
     span {
       position: absolute;
       left: 140px;
     }
     body{line-height: 1.6;}
+    .bottomCentered{
+       position: fixed;   
+       text-align: center;
+       bottom: 30px;
+       width: 100%;
+    }
   </style>
-  
+
    <h1> Application Form </h1>
-   <h3> Page 2: Prior Degrees </h3><br>
+   <h2> Page 2: Prior Degrees </h2>
   
-  <body>
-    
-    <form method="post">
+   <body>
+     <!--change form method to page 3-->
+     <form id="mainform" action="application_form_recomendation.php" method="post">
+      <h3>Degree One (required)</h3>
       Degree Type <br>
       <input type="radio" name="type" value="MS" checked> MS<br>
       <input type="radio" name="type" value="BS"> BS<br>
       <input type="radio" name="type" value="BA"> BA <br>
       GPA <span><input type="text" name="gpa"></span><br>
       Year <span><input type="text" name="year"></span><br>
-      University<span><input type="text" name="university"></span><br><br>
-      
-      <!-- make it go at bottom of page -->
-      <input type="submit" name="submit" value="Submit"> 
+      University<span><input type="text" name="university"></span><br>
+
+      <hr>     
+      <h3>Degree Two (optional)</h3>
+      Degree Type <br>
+      <input type="radio" name="type2" value="MS" checked> MS<br>
+      <input type="radio" name="type2" value="BS"> BS<br>
+      <input type="radio" name="type2" value="BA"> BA <br>
+      GPA <span><input type="text" name="gpa2"></span><br>
+      Year <span><input type="text" name="year2"></span><br>
+      University<span><input type="text" name="university2"></span><br>
+
+      <hr>
+      <h3>Degree Three (optional)</h3>
+      Degree Type <br>
+      <input type="radio" name="type3" value="MS" checked> MS<br>
+      <input type="radio" name="type3" value="BS"> BS<br>
+      <input type="radio" name="type3" value="BA"> BA <br>
+      GPA <span><input type="text" name="gpa3"></span><br>
+      Year <span><input type="text" name="year3"></span><br>
+      University<span><input type="text" name="university3"></span><br>
+
+      <hr>
+      <h3>Degree Four (optional)</h3>
+      Degree Type <br>
+      <input type="radio" name="type4" value="MS" checked> MS<br>
+      <input type="radio" name="type4" value="BS"> BS<br>
+      <input type="radio" name="type4" value="BA"> BA <br>
+      GPA <span><input type="text" name="gpa4"></span><br>
+      Year <span><input type="text" name="year4"></span><br>
+      University<span><input type="text" name="university4"></span><br>
     </form>
-   
-    <form method="post">
-      <input type="submit" name="another" value="Add Another Degree"><br>
-      <?php
-        if (isset($_POST["another"])){
-          $form = '<hr><form method="post">
-                   Degree Type <br>
-                   <input type="radio" name="type" value="MS" checked> MS<br>
-                   <input type="radio" name="type" value="BS"> BS<br>
-                   <input type="radio" name="type" value="BA"> BA <br>
-                   GPA <span><input type="text" name="gpa"></span><br>
-                   Year <span><input type="text" name="year"></span><br>
-                   University<span><input type="text" name="university"></span>></form>';
-          echo $form;
-        }
-      ?>
-    </form>
-      
+
+    <!-- make it go at bottom of page -->
+    <div class="bottomCentered"><input type="submit" name="submit" value="Submit" form="mainform"></div>
   </body>
-</html>
+  
+</html>                                                                                    
