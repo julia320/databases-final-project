d075d99d8351e7e960cb202be454a68ccec282c6
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-18 16:52:18 -0400

    Create application_form_end.php

---
+++ application_form_end.php
@@ -0,0 +1,28 @@
+<html>
+  
+  <title>
+    Application Form
+  </title>
+  
+  <style>
+    span {
+      position: absolute;
+      left: 160px;
+    }
+    body{line-height: 1.6;}
+    .bottomCentered{
+       position: fixed;   
+       text-align: center;
+       bottom: 30px;
+       width: 100%;
+    }
+  </style>
+  
+   <h1> Complete </h1>
+   <h2> You Have Completed Your Application </h2>
+   
+  <body>
+    <div class="bottomCentered"><input type="submit" name="submit" value="Go to Homepage"></div>    
+  </body>
+  
+</html>
