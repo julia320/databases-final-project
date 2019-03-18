008963a2f5b346a2159c1966dbe0a9a43672e975
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-18 18:44:53 -0400

    Update application_form_academicInfo_page1.php

--- application_form_academicInfo_page1.php
+++ application_form_academicInfo_page1.php
@@ -65,7 +65,7 @@
       <input type="radio" name="degreeType" value="MS" > MS<br>
       <input type="radio" name="degreeType" value="PhD"> PhD<br><br>
   
-      Year <span><input type="text" name="appYear"> <span class="error"><?php echo $appYearErr;?></span></span><br>
+      Year <span><input type="text" name="appYear"></span><span class="error"><?php echo $appYearErr;?></span><br>
       
       Semester <br>
       <input type="radio" name="semester" value="fall"> Fall<br>
