241227132638066709431c666e8fb8fad862bb6d
sloanej <46733794+sloanej@users.noreply.github.com>
2019-03-18 20:17:17 -0400

    Update application_form_academicInfo_page1.php

--- application_form_academicInfo_page1.php
+++ application_form_academicInfo_page1.php
@@ -125,10 +125,11 @@
     
     //Insert into database 
     if ($dataReady == true){
+      $uid = 99;
       $sql = "INSERT INTO academic_info (uid, degreeType, AOI, experience, semester, year) 
-              VALUES(99, $degreeType', '$aoi', '$experience', '$semester', '$year')";
+              VALUES('$uid', $degreeType', '$aoi', '$experience', '$semester', '$year')";
       $result = mysqli_query($conn, $sql) or die ("************* SQL FAILED *************");
-      //Check	if query was successful	
+      //Check if query was successful	
       if ($result)	{	
         //Account created - we are ready back to webstore and be logged in
         $done = true;
