51b7c784f5741892c0b0e622c5e4491bd8cc678a
Julia Bristow <julia_bristow@gwu.edu>
2019-03-25 10:48:37 -0400

    fix all syntax errors

--- review.php
+++ review.php
@@ -19,81 +19,66 @@
     <h1>Info for applicant: <?php echo $_GET['uid']?></h1>
     <h3>Recommendation Letters</h3>
     
-    //checking if recommendation letters were submitted
-    
-    <?php
-    $q = "SELECT * FROM rec_letter WHERE uid=?";
-    if(!inDb($q,[$_GET['uid']])) {
-        ?>
-        <p>
-            Recommendation Not Letter received
-        </p>
-        <?php
-    }else{
-        $qr = "SELECT * FROM rec_letter WHERE uid=?";
-        ?>
-        <p>
-            Recommendation Letter received
-        </p>
+
+	<?php
+		// check if recommendation letter was submitted	
+    	$q = "SELECT * FROM rec_letter WHERE uid=?";
+	    if(!inDb($q,[$_GET['uid']])) 
+			echo "<p>Recommendation Not Letter received</p>";
+
+	    else {
+    	    $qr = "SELECT * FROM rec_letter WHERE uid=?";
+        	echo "<p>Recommendation Letter received</p>";
         
-        // review recommendation letters
-        
-        <?php
-        if(!inDb($qr,[$_GET['uid']])) {
-            ?>
-            <p>
-                <a href="rec_review.php?uid=<?php echo $_GET['uid']; ?>"> Review Recommendation Letter</a>
-            </p>
-            <?php
-        }else{
-            ?>
-            <p>
-                Recommendation letter has been review
-            </p>
-            <?php
-        }
-    }
-    ?>
+	        // review recommendation letters
+    	    if(!inDb($qr,[$_GET['uid']])) 
+	    	   	echo "<p><a href='rec_review.php?uid=".$_GET['uid']."'>Review Recommendation Letter</a></p>";
+	        else
+    	        echo "<p>Recommendation letter has been reviewed</p>";
+    	}
+	?>
+
+
     <h3>Transcripts</h3>
 
-//display scores
+	<?php
+   		// display scores
+		$query="SELECT * FROM gre WHERE uid=?";
 
-    <?php
-    $query="SELECT * FROM gre WHERE uid=?";
-    if(inDb($query,[$_GET['uid']])) {
-        echo "
-        <table border="1" style="border-collapse: collapse;">
-            <tr>
-                <th>Year</th>
-                <th>Verbal</th>
-                <th>Quantity</th>
-                <th>AdvScore</th>
-                <th>toefl</th>
-                <th>advYear</th>
-            </tr>"
-            
+		// if scores have been entered, display them
+    	if(inDb($query,[$_GET['uid']])) {
+        	echo "<table border='1' style='border-collapse: collapse;'>
+            	<tr>
+                	<th>Year</th>
+	                <th>Verbal</th>
+    	            <th>Quantity</th>
+        	        <th>AdvScore</th>
+            	    <th>toefl</th>
+                	<th>advYear</th>
+				</tr>";
+		        
             $rows = fetchRows($query,[$_GET['uid']]);
             foreach ($rows as $row){
-                echo "
-                <tr>
-                    <td><?php echo $row['advyear'];?></td>
-                    <td><?php echo $row['verbal'];?></td>
-                    <td><?php echo $row['quant'];?></td>
-                    <td><?php echo $row['advScore'];?></td>
-                    <td><?php echo $row['toefl'];?></td>
-                    <td><?php echo $row['advYear'];?></td>
-                </tr>"
+                echo "<tr>
+                    	<td>".$row['advyear']."</td>
+	                    <td>".$row['verbal']."/td>
+    	                <td>".$row['quant']."</td>
+        	            <td>".$row['advScore']."</td>
+            	        <td>".$row['toefl']."</td>
+                	    <td>".$row['advYear']."</td>
+	                </tr>";
             }
         
-        echo "</table>"
+        	echo "</table>";
         
-    }else{
-        echo "<p>No Transcript received</p>"
-    }
+		}
+		else {
+        	echo "<p>No Transcript received</p>";
+   		}
 
-    if(inDb("SELECT * FROM gre WHERE uid=?",[$_GET['uid']])&&inDb("SELECT * FROM rec_review WHERE uid=?",[$_GET['uid']])){
-        echo "<p><a href='results.php?uid=<?php echo $_GET['uid'];?>''>Record application results</a></p>"
-    }
+    	if(inDb("SELECT * FROM gre WHERE uid=?",[$_GET['uid']])&&inDb("SELECT * FROM rec_review WHERE uid=?",[$_GET['uid']])){
+        	echo "<p><a href='results.php?uid=".$_GET['uid']."'>Record application results</a></p>";
+		}
     ?>
 
 </body>
