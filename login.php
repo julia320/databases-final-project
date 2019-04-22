413409f637384aba699591364fadb65a2d91b1b6
searri <searri@gwu.edu>
2019-04-22 00:59:13 +0000

    Uncomment favicon

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
 <head>
 	<title>ARGv Login</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<!-- <link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" /> -->
+	<link rel="icon" type="image/png" href="images/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
 
 	<style>
 		/* 2 columns for logging in/signing up */
