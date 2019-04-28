66075508aa3cd1a1ac73e5768346464f4f06b64f
Julia Bristow <julia_bristow@gwu.edu>
2019-04-28 18:20:51 -0400

    formatting

--- Advisor.php
+++ Advisor.php
@@ -1,7 +1,4 @@
 <!DOCTYPE html>
-<?php
-session_start();
-?>
 <html lang="en" dir="ltr">
 <head>
   <meta charset="utf-8">
@@ -10,162 +7,123 @@ session_start();
   <link rel="icon" type="image/png" href="images/favicon-16x16.png" sizes="16x16" />
   <link rel = "stylesheet" type="text/css" href="style.css"/>
 </head>
+
 <body>
-  <div style="display: inline-block;" class="menu-button">
-    <form action="menu.php"><input type="submit" value="Menu"/></form>
-  </div>
+    <div style="display: inline-block;" class="menu-button">
+        <form action="menu.php"><input type="submit" value="Menu"/></form>
+    </div>
 
-  <h2>Advisor Page</h2>
-  <hr>
-  <?php
+    <h2>Advisor Page</h2>
+    <hr>
 
-  $conn = mysqli_connect("localhost", "ARGv", "CSCI2541_sp19", "ARGv");
+    <?php
+    session_start();
+    $conn = mysqli_connect("localhost", "ARGv", "CSCI2541_sp19", "ARGv");
+    if($mysqli->connect_error) 
+        die("Connection failed: " . mysqli_connect_error());
 
-  if($mysqli->connect_error) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
+    //advisors uid
+    $aid = $_SESSION['uid']; 
 
-
-  //advisors uid
-  $aid = $_SESSION['uid']; //advisors uid
-
-  if(empty($_SESSION['uid']))
-    $aid = $_POST['searchID1'];
-  else if($_SESSION['uid'] == 0)
-    $aid = $_POST['searchID1'];
-
-  if($_SESSION['uid'] == 0)
-    {?>
-          <!-- <form class="back" action="<?php //echo $_SERVER["PHP_SELF"];?>" method="post">
-        <input type="submit" value="Back" formaction="admin.php">
-      </form> -->
-      <?php
-    }
-    else
-      {?>
-          <!-- <form class="back" action="<?php //echo $_SERVER["PHP_SELF"];?>" method="post">
-        <input type="submit" value="Back" formaction="Advisor.php">
-      </form> -->
-      <?php
-    }
+    if(empty($_SESSION['uid']))
+        $aid = $_POST['searchID1'];
+    else if($_SESSION['uid'] == 0)
+        $aid = $_POST['searchID1'];
 
 
     // Advisor views students
     $query = "SELECT * FROM user WHERE advisor=".$aid." AND (type='PHD' OR type='MS')";
     $result = mysqli_query($conn, $query);
-    if ($result->num_rows == 0) {
+    if ($result->num_rows == 0) 
       echo "<p>You do not have any advisees.</p>";
-    }
 
-    while ($row = mysqli_fetch_assoc($result)){
+    while ($row = mysqli_fetch_assoc($result)) {
       ?>
-      <form class="student-info" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
-        <div class="student-info">
-          <?php echo $row["uid"] . " " . $row["fname"] . " " . $row["lname"]; ?>
-          <input hidden type="text" name="uid" value="<?php echo $row['uid']; ?>">
-          <input type="submit" name="form1" value="View Form1">
-          <!-- <input type="submit" name="Transcript" value="View Transcript"> -->
-          <?php
-          if ($row['program'] == 'PHD'){
-            ?>
-            <input type="submit" name="Thesis" value="Approve Thesis">
-            <?php
-
-          }
-          ?>
-
-        </div>
-      </form>
-
-
-      <?php
-
-    }
-
-			// Advisor views alumni
-			$aid = $_SESSION['uid']; //advisors uid
-      $query = "SELECT * FROM user WHERE advisor=".$aid." AND type='alum')";
-      $result = mysqli_query($conn, $query);
-      while ($row = mysqli_fetch_assoc($result)){
-        ?>
         <form class="student-info" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
-          <div class="student-info">
+            <div class="student-info">
             <?php echo $row["uid"] . " " . $row["fname"] . " " . $row["lname"]; ?>
-            <input hidden type="text" name="uid" value="<?php echo $row["uid"]; ?>">
+            <input hidden type="text" name="uid" value="<?php echo $row['uid']; ?>">
             <input type="submit" name="form1" value="View Form1">
-            <!-- <input type="submit" name="Transcript" value="View Transcript"> -->
+          <?php
+            if ($row['program'] == 'PHD') {
+                echo "<input type='submit' name='Thesis' value='Approve Thesis'>";
+            }
+          ?>
+            </div>
+        </form>
+    <?php
+    }// End: viewing students
 
-            <?php
-            ?>
 
-          </div>
+	// Advisor views alumni
+	$aid = $_SESSION['uid']; //advisors uid
+    $query = "SELECT * FROM user WHERE advisor=".$aid." AND type='alum')";
+    $result = mysqli_query($conn, $query);
+    while ($row = mysqli_fetch_assoc($result)){
+    ?>
+        <form class="student-info" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+            <div class="student-info">
+                <?php echo $row["uid"] . " " . $row["fname"] . " " . $row["lname"]; ?>
+                <input hidden type="text" name="uid" value="<?php echo $row["uid"]; ?>">
+                <input type="submit" name="form1" value="View Form1">
+            </div>
         </form>
 
 
-        <?php
+    <?php
+    }// End: viewing alumni
 
-      }
 
 
-      if(isset($_POST['form1'])){
-       ?>
-       <form class="student-info" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
-        <input type="submit" name="Back" value="Back">
-      </form>
-      <?php
-      $uid = $_POST['uid'];
-      $query = "SELECT u_id, form1.crn, dept, semester, year FROM form1, course WHERE u_id =".$uid." AND form1.crn=course.crn";
-      $result = mysqli_query($conn, $query) or die ("Couldn't find course: ".mysql_error($conn));
-
-      while ($row = mysqli_fetch_assoc($result)){
+    if(isset($_POST['form1'])){
         ?>
-        <br>
-        <div class="form1-info">
-
-
-          <?php
-          echo "CRN: ".$row["form1.crn"]." Department: ".$row["dept"]." Semester: ".$row["semester"]." Year: ".$row['year']."<br/>";
-          ?>
-        </div>
+        <form class="student-info" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+            <input type="submit" name="Back" value="Back">
+        </form>
         <?php
-      }
+        $uid = $_POST['uid'];
+        $query = "SELECT u_id, form1.crn, dept, semester, year FROM form1, course WHERE u_id =".$uid." AND form1.crn=course.crn";
+        $result = mysqli_query($conn, $query) or die ("Couldn't find course: ".mysql_error($conn));
 
-
+        echo "<div class='form1-info'>";
+        while ($row = mysqli_fetch_assoc($result)){
+            echo "<br/>CRN: ".$row["form1.crn"]." Department: ".$row["dept"]." Semester: ".$row["semester"]." Year: ".$row['year']."<br/>";
+        }
+        echo "</div>";
     }
+//}
 
 
     if(isset($_POST["Thesis"])){
-    ?>
-      <form class="student-info" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
-        <input type="submit" name="Back" value="Back">
-      </form>
+        ?>
+        <form class="student-info" action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+            <input type="submit" name="Back" value="Back">
+        </form>
 
-      <?php
-      $query = "UPDATE thesis_status SET status = 'passed' WHERE u_id=".$uid;
-      $result = mysqli_query($conn, $query);
-      echo "Thesis approved";
+        <?php
+        $query = "UPDATE thesis_status SET status = 'passed' WHERE u_id=".$uid;
+        $result = mysqli_query($conn, $query);
+        echo "Thesis approved";
     }
 
 
-  if(isset($_POST['Transcript'])){
-    $uid = $_POST['uid'];
-    $query = "SELECT * FROM transcript, course WHERE uid=".$uid." AND course.crn=transcript.crn";
-    $result = mysqli_query($conn, $query) or die ("Error fetching transcript: ".mysqli_error($conn));
-    while ($row = mysqli_fetch_assoc($result)){
-     ?>
-     <br>
-     <div class="Transcript-info">
+    if(isset($_POST['Transcript'])){
+        $uid = $_POST['uid'];
+        $query = "SELECT * FROM transcript, course WHERE uid=".$uid." AND course.crn=transcript.crn";
+        $result = mysqli_query($conn, $query) or die ("Error fetching transcript: ".mysqli_error($conn));
+        while ($row = mysqli_fetch_assoc($result)){
+            ?>
+            <br>
+            <div class="Transcript-info">
 
-       <?php
-       echo "Course: ".$row["name"]." Department: ".$row["dept"]." Semester: " . $row["semester"]." Year: ".$row['year']." Credits: ".$row["credits"]." Grade: ".$row["grade"]."<br/>";
-       ?>
-     </div>
-     <?php
-   }
-
- }
-
- ?>
+             <?php
+             echo "Course: ".$row["name"]." Department: ".$row["dept"]." Semester: " . $row["semester"]." Year: ".$row['year']." Credits: ".$row["credits"]." Grade: ".$row["grade"]."<br/>";
+             ?>
+            </div>
+         <?php
+        }
+    }
+?>
 
 </body>
 </html>
