4cb449f1d83228e8881268ade1f8912bc0c93832
searri <searri@gwu.edu>
2019-05-04 15:41:29 +0000

    Update menu page

--- student-lists.php
+++ student-lists.php
@@ -40,13 +40,14 @@
             echo '<option value="PHD">PhD</option>';
             echo '<option value="MS">Masters</option>';
             echo '</select>';
-
+            echo 'OR';
             echo '<select name="year">';
             echo '<option value="" disabled selected>Select Admit Year</option>';
-            $query = "select fname, lname, uid, email, active, hold from user where type='MS' or type='PHD' order by lname";
+            $query = "select distinct dated from academic_info where uid in (SELECT DISTINCT R.uid FROM app_review R, user U WHERE R.uid=U.uid AND (type='App' or type='MS' or type='PHD') AND status IN (6,7))";
             $result = mysqli_query($connection, $query);
-            echo '<option value="PHD">PhD</option>';
-            echo '<option value="MS">Masters</option>';
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo '<option value="PHD">'.$row['dated'].'</option>';
+            }
             echo '</select>';
             
             echo '<input type="submit" name="admitted" value="View ADMITTED Students"/>';
@@ -55,23 +56,23 @@
 
             //no UID search
             if(empty($_POST["search"])) {
-            $query = "select fname, lname, uid, email, active, hold from user where type='MS' or type='PHD' order by lname";
-            $result = mysqli_query($connection, $query);
-            if (mysqli_num_rows($result) > 0) {
-                echo "<table>";
-                //Display a table of all the students
-                echo "<thead><tr><th>First Name</th><th>Last Name</th><th>UID</th><th>Email</th><th>Active</th><th>Hold Status</th></tr></thead>";
-                while ($row = mysqli_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $row["fname"] . "</td>";
-                    echo "<td>" . $row["lname"] . "</td>";
-                    echo "<td>" . $row["uid"] . "</td>";
-                    echo "<td>" . $row["email"] . "</td>";
-                    echo "<td>" . $row["active"] . "</td>";
-                    echo "<td>" . $row["hold"] . "</td>";
-                    echo "</tr>";
-                }
-                echo "</table>";
+                $query = "select fname, lname, uid, email, active, hold from user where type='MS' or type='PHD' order by lname";
+                $result = mysqli_query($connection, $query);
+                if (mysqli_num_rows($result) > 0) {
+                    echo "<table>";
+                    //Display a table of all the students
+                    echo "<thead><tr><th>First Name</th><th>Last Name</th><th>UID</th><th>Email</th><th>Active</th><th>Hold Status</th></tr></thead>";
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo "<tr>";
+                        echo "<td>" . $row["fname"] . "</td>";
+                        echo "<td>" . $row["lname"] . "</td>";
+                        echo "<td>" . $row["uid"] . "</td>";
+                        echo "<td>" . $row["email"] . "</td>";
+                        echo "<td>" . $row["active"] . "</td>";
+                        echo "<td>" . $row["hold"] . "</td>";
+                        echo "</tr>";
+                    }
+                    echo "</table>";
 
                 } else {
                     //If nothing came back from the query, there was a problem
