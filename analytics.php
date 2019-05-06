69e37e893dd7e2d212b5532ac16c8ee28d93234a
searri <searri@gwu.edu>
2019-05-06 03:58:10 +0000

    Finishing touches on data mining page

--- analytics.php
+++ analytics.php
@@ -48,107 +48,109 @@
             //connect to database
             $connection = mysqli_connect("localhost", "ARGv", "CSCI2541_sp19", "ARGv");
 
-            //determine number of entries
-            $getEntries = "select count(distinct uid) from transcript";
-            $numEntries = mysqli_fetch_assoc(mysqli_query($connection, $getEntries))['count(distinct uid)'];
+            //Time to do some data mining! Get parameters from user
+            if(isset($_POST['timeToMine'])) {
 
-            //setup association array
-            $getCRNS = "select count(distinct crn) from transcript";
-            $arrDim = mysqli_fetch_assoc(mysqli_query($connection, $getCRNS))['count(distinct crn)'];
-            $assoc_arr = array_fill(0, $arrDim, array_fill(0, $arrDim, 0));
-            
-            //map array indices
-            $getCRNS = "select distinct crn from transcript";
-            $CRNsResultSet = mysqli_query($connection, $getCRNS);
-            $counter = 0;
-            while($row = mysqli_fetch_assoc($CRNsResultSet)) {
-                $arr_map[$row['crn']] = $counter;
-                $counter++;
-            }
+                //determine number of entries
+                $getEntries = "select count(distinct uid) from transcript";
+                $numEntries = mysqli_fetch_assoc(mysqli_query($connection, $getEntries))['count(distinct uid)'];
 
-            //get list of student IDs from table
-            $getUIDS = "select distinct uid from transcript";
-            $UIDsResult = mysqli_query($connection, $getUIDS);
-            $UIDsInTranscript = [];
-            while($row = mysqli_fetch_assoc($UIDsResult)) {
-                $UIDsInTranscript[] = $row;
-            }
-            
-            //Assemble list of courses that each student is taking
-            $coursesList = [];
-            for($i = 0; $i<$numEntries; $i++) {
-                $getCourses = "select crn from transcript where uid=".$UIDsInTranscript[$i]['uid'];
-                // echo $query."<br>";
-                $coursesList[$i] = [];
-                $courseSet = mysqli_query($connection, $getCourses);
-                while($row = mysqli_fetch_assoc($courseSet)) {
-                    $coursesList[$i][] = $row;
+                //setup association array
+                $getCRNS = "select count(distinct crn) from transcript";
+                $arrDim = mysqli_fetch_assoc(mysqli_query($connection, $getCRNS))['count(distinct crn)'];
+                $assoc_arr = array_fill(0, $arrDim, array_fill(0, $arrDim, 0));
+                
+                //map array indices
+                $getCRNS = "select distinct crn from transcript";
+                $CRNsResultSet = mysqli_query($connection, $getCRNS);
+                $counter = 0;
+                while($row = mysqli_fetch_assoc($CRNsResultSet)) {
+                    $arr_map[$row['crn']] = $counter;
+                    $counter++;
                 }
-            }
 
-            //DEBUG ONLY - PRINT COURSE LIST
-            // for($i = 0; $i<$numEntries; $i++) {
-            //     echo "<b>".$UIDsInTranscript[$i]['uid'].": </b>";
-            //     for($j = 0; $j<count($coursesList[$i]); $j++) {
-            //         $q = "select name from course where crn=".$coursesList[$i][$j]['crn'];
-            //         echo mysqli_fetch_assoc(mysqli_query($connection, $q))['name'].", ";
-            //     }
-            //     echo "<br>";
-            // }
-
-            //Populate association matrix
-            for($i = 0; $i<$numEntries; $i++) {
-
-                //This logic handles finding all combinations of courses within a single student
-                //That is, everything except the middle diagonal
-                $alreadyProcessed = array();
-                foreach ($coursesList[$i] as $first) {
-                    // Loop the array twice (as @Dagon mentioned)
-                    foreach ($coursesList[$i] as $second) {
-                        // Keep track of what you've already processed
-                        $combination = array($first['crn'], $second['crn']);
-                        // Sorting the numbers will ensure that 2 - 1 and 1 - 2 are the same
-                        sort($combination);
-                        // Ensure they aren't the same number and you haven't already processed them
-                        if ($first === $second || in_array($combination, $alreadyProcessed)) {
-                            continue;
-                        }
-
-                        //now first and second are the coordinates in the association matrix
-                        $assoc_arr[$arr_map[$first['crn']]][$arr_map[$second['crn']]]++;
-                        $assoc_arr[$arr_map[$second['crn']]][$arr_map[$first['crn']]]++;
-
-                        // Add it to the list of what you've already processed
-                        $alreadyProcessed[] = $combination;
+                //get list of student IDs from table
+                $getUIDS = "select distinct uid from transcript";
+                $UIDsResult = mysqli_query($connection, $getUIDS);
+                $UIDsInTranscript = [];
+                while($row = mysqli_fetch_assoc($UIDsResult)) {
+                    $UIDsInTranscript[] = $row;
+                }
+                
+                //Assemble list of courses that each student is taking
+                $coursesList = [];
+                for($i = 0; $i<$numEntries; $i++) {
+                    $getCourses = "select crn from transcript where uid=".$UIDsInTranscript[$i]['uid'];
+                    // echo $query."<br>";
+                    $coursesList[$i] = [];
+                    $courseSet = mysqli_query($connection, $getCourses);
+                    while($row = mysqli_fetch_assoc($courseSet)) {
+                        $coursesList[$i][] = $row;
                     }
                 }
 
-                //Populate the middle diagonal
-                foreach ($coursesList[$i] as $justAnArr) {
-                    $assoc_arr[$arr_map[$justAnArr['crn']]][$arr_map[$justAnArr['crn']]]++;
+                //DEBUG ONLY - PRINT COURSE LIST
+                // for($i = 0; $i<$numEntries; $i++) {
+                //     echo "<b>".$UIDsInTranscript[$i]['uid'].": </b>";
+                //     for($j = 0; $j<count($coursesList[$i]); $j++) {
+                //         $q = "select name from course where crn=".$coursesList[$i][$j]['crn'];
+                //         echo mysqli_fetch_assoc(mysqli_query($connection, $q))['name'].", ";
+                //     }
+                //     echo "<br>";
+                // }
+
+                //Populate association matrix
+                for($i = 0; $i<$numEntries; $i++) {
+
+                    //This logic handles finding all combinations of courses within a single student
+                    //That is, everything except the middle diagonal
+                    $alreadyProcessed = array();
+                    foreach ($coursesList[$i] as $first) {
+                        // Loop the array twice (as @Dagon mentioned)
+                        foreach ($coursesList[$i] as $second) {
+                            // Keep track of what you've already processed
+                            $combination = array($first['crn'], $second['crn']);
+                            // Sorting the numbers will ensure that 2 - 1 and 1 - 2 are the same
+                            sort($combination);
+                            // Ensure they aren't the same number and you haven't already processed them
+                            if ($first === $second || in_array($combination, $alreadyProcessed)) {
+                                continue;
+                            }
+
+                            //now first and second are the coordinates in the association matrix
+                            $assoc_arr[$arr_map[$first['crn']]][$arr_map[$second['crn']]]++;
+                            $assoc_arr[$arr_map[$second['crn']]][$arr_map[$first['crn']]]++;
+
+                            // Add it to the list of what you've already processed
+                            $alreadyProcessed[] = $combination;
+                        }
+                    }
+
+                    //Populate the middle diagonal
+                    foreach ($coursesList[$i] as $justAnArr) {
+                        $assoc_arr[$arr_map[$justAnArr['crn']]][$arr_map[$justAnArr['crn']]]++;
+                    }
                 }
-            }
-            $assoc_arr[0][0]--;
+                $assoc_arr[0][0]--;
 
-            //DEBUG: Print association matrix as an aligned rectangle rather than the PHP mess
-            // header('Content-Type: text/plain');
-            // echo "\n";
-            // for($i=0; $i<$arrDim; $i++) {
-            //     for($j=0; $j<$arrDim; $j++) {
-            //         echo $assoc_arr[$i][$j];
-            //         echo "\t";
-            //     }
-            //     echo "\n";
-            // }
+                //DEBUG: Print association matrix as an aligned rectangle rather than the PHP mess
+                // header('Content-Type: text/plain');
+                // echo "\n";
+                // for($i=0; $i<$arrDim; $i++) {
+                //     for($j=0; $j<$arrDim; $j++) {
+                //         echo $assoc_arr[$i][$j];
+                //         echo "\t";
+                //     }
+                //     echo "\n";
+                // }
 
-            //Time to do some data mining! Get parameters from user
-            if(isset($_POST['timeToMine'])) {
+
                 $support = $_POST['support']/100.0;
                 $confidence = $_POST['confidence']/100.0;
                 $foundNone = true;
-                echo "<br><br>";
-                echo "Analyzing course assocations with desired support of ".$_POST['support']."% and desired confidence of ".$_POST['confidence']."%:";
-                echo "<ul>";
+                echo "<br>";
+                $_SESSION['currStats'] = "Analyzing course assocations with desired support of ".$_POST['support']."% and desired confidence of ".$_POST['confidence']."%.";
+                // echo "<ul>";
                 $associations = [];
                 $counter = 0;
 
@@ -164,30 +166,81 @@
                                 //if confidence passes, print association
                                 if($assoc_arr[$i][$j]/$assoc_arr[$i][$i] >= $confidence) {
                                     $foundNone = false;
-                                    $getAssocC1 = "select courseno, dept, name from course where crn=".array_search($i, $arr_map);
-                                    $assocResC1 = mysqli_query($connection, $getAssocC1);
-                                    $getAssocC2 = "select courseno, dept, name from course where crn=".array_search($j, $arr_map);
-                                    $assocResC2 = mysqli_query($connection, $getAssocC2);
-                                    echo "<li>";
-                                    while($row = mysqli_fetch_assoc($assocResC1)) {
-                                        echo $row['dept']." ".$row['courseno'].": ".$row['name'];
-                                    }
-                                    echo " ⇒ ";
-                                    while($row = mysqli_fetch_assoc($assocResC2)) {
-                                        echo $row['dept']." ".$row['courseno'].": ".$row['name'];
-                                    }
-                                    echo "</li>";
+                                    $associations[$counter] = array(array_search($i, $arr_map), array_search($j, $arr_map));
+                                    $counter++;
                                 }
                             }
                         }
                     }
                 }
-                echo "</ul>";
+                // echo "</ul>";
                 // echo $support;
                 // echo $confidence;
+                $_SESSION['associations'] = $associations;
                 if($foundNone) {
                     echo "No associations were found with the specified confidence and support levels.";
+                    die();
                 }
+            }
+
+            echo $_SESSION['currStats'];
+
+            //search box
+            echo "<div style=\"text-align: center;\">";
+            echo "<form method=\"post\" action=\"analytics.php\"><input type=\"text\" name=\"search\" placeholder=\"Search for Course A by name...\"/><input type=\"submit\" value=\"Search\"/>";
+            echo "</form></div>";
+            echo "<br>";
+            $search = $_POST['search'];
+
+            //build table of results
+            if (is_null($search)) {
+                echo "<table>";
+                echo "<thead><tr><th>Course A</th><th> ⇒ </th><th>Course B</th></tr></thead>";
+                for($i=0; $i<count($_SESSION['associations']); $i++) {
+                    $getAssocC1 = "select courseno, dept, name from course where crn=".$_SESSION['associations'][$i][0];
+                    $assocResC1 = mysqli_query($connection, $getAssocC1);
+                    $getAssocC2 = "select courseno, dept, name from course where crn=".$_SESSION['associations'][$i][1];
+                    $assocResC2 = mysqli_query($connection, $getAssocC2);
+                    echo "<tr>";
+                    while($row = mysqli_fetch_assoc($assocResC1)) {
+                        echo "<td>";
+                        echo $row['dept']." ".$row['courseno'].": ".$row['name'];
+                        echo "</td>";
+                    }
+                    echo "<td> ⇒ </td>";
+                    while($row = mysqli_fetch_assoc($assocResC2)) {
+                        echo "<td>";
+                        echo $row['dept']." ".$row['courseno'].": ".$row['name'];
+                        echo "</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<table>";
+                echo "<thead><tr><th>Course A</th><th> ⇒ </th><th>Course B</th></tr></thead>";
+                for($i=0; $i<count($_SESSION['associations']); $i++) {
+                    $getAssocC1 = "select courseno, dept, name from course where crn=".$_SESSION['associations'][$i][0]." and name like'%".$search."%'";
+                    $assocResC1 = mysqli_query($connection, $getAssocC1);
+                    $getAssocC2 = "select courseno, dept, name from course where crn=".$_SESSION['associations'][$i][1];
+                    $assocResC2 = mysqli_query($connection, $getAssocC2);
+                    if(mysqli_num_rows($assocResC1) > 0) {
+                        echo "<tr>";
+                        while($row = mysqli_fetch_assoc($assocResC1)) {
+                            echo "<td>";
+                            echo $row['dept']." ".$row['courseno'].": ".$row['name'];
+                            echo "</td>";
+                        }
+                        echo "<td> ⇒ </td>";
+                        while($row = mysqli_fetch_assoc($assocResC2)) {
+                            echo "<td>";
+                            echo $row['dept']." ".$row['courseno'].": ".$row['name'];
+                            echo "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                }
+                echo "</table>";
             }
 
             //close sql connection
